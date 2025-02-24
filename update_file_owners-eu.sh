ba18cfbe45005aa8b8fc00873a1f51fc04776208
Dots <dotvnl@gmail.com>
2025-02-24 18:34:03 +0200

    basic chown scripts for folders i need to change owner on often

---
+++ update_file_owners-eu.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# MISC
+
+sudo chown -R webmaster:webmaster /home/webmaster/
+sudo chown -R webmaster:webmaster /var/www/
+
+sudo chown -R web-misc:web-misc /home/web-misc/
+
+sudo chown -R steam:steam /home/steam/
+
+# DISCORD
+
+sudo chown -R discord-fkz-1:discord-fkz-1 /home/discord-fkz-1/
+
+sudo chown -R discord-fkz-2:discord-fkz-2 /home/discord-fkz-2/
+
+sudo chown -R discord-fkz-3:discord-fkz-3 /home/discord-fkz-3/
+
+# MC
+
+sudo chown -R mc-dot-1:mc-dot-1 /home/mc-dot-1/
+
+sudo chown -R mc-iwaki-1:mc-iwaki-1 /home/mc-iwaki-1/
+
+# CS2
+
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /home/cs2-fkz-1/
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files.femboy.kz/cs2/demos/fkz-1/
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files.femboy.kz/cs2/maps/
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files.femboy.kz/cs2/workshop/
+
+sudo chown -R cs2-fkz-2:cs2-fkz-2 /home/cs2-fkz-2/
+sudo chown -R cs2-fkz-2:cs2-fkz-2 /var/www/files.femboy.kz/cs2/demos/fkz-2/
+
+sudo chown -R cs2-fkz-3:cs2-fkz-3 /home/cs2-fkz-3/
+sudo chown -R cs2-fkz-3:cs2-fkz-3 /var/www/files.femboy.kz/cs2/demos/fkz-3/
+
+sudo chown -R cs2-fkz-4:cs2-fkz-4 /home/cs2-fkz-4/
+sudo chown -R cs2-fkz-4:cs2-fkz-4 /var/www/files.femboy.kz/cs2/demos/fkz-4/
+
+sudo chown -R cs2-fkz-5:cs2-fkz-5 /home/cs2-fkz-5/
+sudo chown -R cs2-fkz-5:cs2-fkz-5 /var/www/files.femboy.kz/cs2/demos/fkz-5/
+
+sudo chown -R cs2-fkz-api:cs2-fkz-api /home/cs2-fkz-api/
+
+# CSGO
+
+sudo chown -R csgo-fkz-1:csgo-fkz-1 /home/csgo-fkz-1/
+sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-1/
+sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-1/
+
+sudo chown -R csgo-fkz-2:csgo-fkz-2 /home/csgo-fkz-2/
+sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-2/
+sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-2/
+
+sudo chown -R csgo-fkz-3:csgo-fkz-3 /home/csgo-fkz-3/
+sudo chown -R csgo-fkz-3:csgo-fkz-3 /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-3/
+sudo chown -R csgo-fkz-3:csgo-fkz-3 /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-3/
+
+sudo chown -R csgo-luf-1:csgo-luf-1 /home/csgo-luf-1/
+sudo chown -R csgo-luf-1:csgo-luf-1 /var/www/files.femboy.kz/fastdl/replays/gokz/luf-1/
+sudo chown -R csgo-luf-1:csgo-luf-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/luf-1/
+
+sudo chown -R csgo-nikita-1:csgo-nikita-1 /home/csgo-nikita-1/
+sudo chown -R csgo-nikita-1:csgo-nikita-1 /var/www/files.femboy.kz/fastdl/replays/gokz/nikita-1/
+sudo chown -R csgo-nikita-1:csgo-nikita-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/nikita-1/
+
+sudo chown -R csgo-salad-1:csgo-salad-1 /home/csgo-salad-1/
+sudo chown -R csgo-salad-1:csgo-salad-1 /var/www/files.femboy.kz/fastdl/replays/gokz/fruity-1/
+sudo chown -R csgo-salad-1:csgo-salad-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/fruity-1/
+
+sudo chown -R csgo-salad-2:csgo-salad-2 /home/csgo-salad-2/
+sudo chown -R csgo-salad-2:csgo-salad-2 /var/www/files.femboy.kz/fastdl/replays/gokz/fruity-2/
+sudo chown -R csgo-salad-2:csgo-salad-2 /var/www/files.femboy.kz/fastdl/replays/kztimer/fruity-2/
+
+sudo chown -R csgo-somali-1:csgo-somali-1 /home/csgo-somali-1/
+sudo chown -R csgo-somali-1:csgo-somali-1 /var/www/files.femboy.kz/fastdl/replays/gokz/somali-1/
+sudo chown -R csgo-somali-1:csgo-somali-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/somali-1/
+
+sudo chown -R csgo-somali-2:csgo-somali-2 /home/csgo-somali-2/
+sudo chown -R csgo-somali-2:csgo-somali-2 /var/www/files.femboy.kz/fastdl/replays/gokz/somali-2/
+sudo chown -R csgo-somali-2:csgo-somali-2 /var/www/files.femboy.kz/fastdl/replays/kztimer/somali-2/
+
+# CSCL
+
+sudo chown -R cscl-fkz-1:cscl-fkz-1 /home/cscl-fkz-1/
+sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files.femboy.kz/cscl/replays/gokz/fkz-1/
+sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-1/
+
+sudo chown -R cscl-fkz-2:cscl-fkz-2 /home/cscl-fkz-2/
+sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files.femboy.kz/cscl/replays/gokz/fkz-2/
+sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-2/
+
+sudo chown -R cscl-fkz-3:cscl-fkz-3 /home/cscl-fkz-3/
+sudo chown -R cscl-fkz-3:cscl-fkz-3 /var/www/files.femboy.kz/cscl/replays/gokz/fkz-3/
+sudo chown -R cscl-fkz-3:cscl-fkz-3 /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-3/
+
+echo "Ownership update complete."
---
+++ update_file_owners-na.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# MISC
+
+sudo chown -R webmaster:webmaster /home/webmaster/
+sudo chown -R webmaster:webmaster /var/www/
+
+sudo chown -R web-listen:web-listen /home/web-listen/
+
+sudo chown -R debian:debian /home/debian/
+
+# DISCORD
+
+# MC
+
+# CS2
+
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /home/cs2-fkz-1/
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files-na.femboy.kz/cs2/demos/fkz-1/
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files-na.femboy.kz/cs2/maps/
+sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files-na.femboy.kz/cs2/workshop/
+
+sudo chown -R cs2-fkz-2:cs2-fkz-2 /home/cs2-fkz-2/
+sudo chown -R cs2-fkz-2:cs2-fkz-2 /var/www/files-na.femboy.kz/cs2/demos/fkz-2/
+
+sudo chown -R cs2-fkz-3:cs2-fkz-3 /home/cs2-fkz-3/
+sudo chown -R cs2-fkz-3:cs2-fkz-3 /var/www/files-na.femboy.kz/cs2/demos/fkz-3/
+
+# CSGO
+
+sudo chown -R csgo-fkz-1:csgo-fkz-1 /home/csgo-fkz-1/
+sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files-na.femboy.kz/fastdl/replays/gokz/fkz-1/
+sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files-na.femboy.kz/fastdl/replays/kztimer/fkz-1/
+
+sudo chown -R csgo-fkz-2:csgo-fkz-2 /home/csgo-fkz-2/
+sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files-na.femboy.kz/fastdl/replays/gokz/fkz-2/
+sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files-na.femboy.kz/fastdl/replays/kztimer/fkz-2/
+
+# CSCL
+
+sudo chown -R cscl-fkz-1:cscl-fkz-1 /home/cscl-fkz-1/
+sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files-na.femboy.kz/cscl/replays/gokz/fkz-1/
+sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files-na.femboy.kz/cscl/replays/kztimer/fkz-1/
+
+sudo chown -R cscl-fkz-2:cscl-fkz-2 /home/cscl-fkz-2/
+sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files-na.femboy.kz/cscl/replays/gokz/fkz-2/
+sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files-na.femboy.kz/cscl/replays/kztimer/fkz-2/
+
+echo "Ownership update complete."
