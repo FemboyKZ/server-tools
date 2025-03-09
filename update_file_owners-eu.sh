be68345ea75b89d73b0e3a8b6959e0ee989a5fe4
Dots <dotvnl@gmail.com>
2025-03-09 20:32:09 +0200

    use find to check for wrong owners instead of setting owner of everything

--- update_file_owners-eu.sh
+++ update_file_owners-eu.sh
@@ -2,98 +2,98 @@
 
 # MISC
 
-sudo chown -R webmaster:webmaster /home/webmaster/
-sudo chown -R webmaster:webmaster /var/www/
+sudo find /home/webmaster -not -user webmaster -not -group webmaster -print -exec chown webmaster:webmaster {} \;
+sudo find /var/www -not -user webmaster -not -group webmaster -not -path '/var/www/files.femboy.kz/cs2/demos*' -not -path '/var/www/files.femboy.kz/cs2/maps*' -not -path '/var/www/files.femboy.kz/cs2/workshop*' -not -path '/var/www/files.femboy.kz/cscl/replays*' -not -path '/var/www/files.femboy.kz/fastdl/replays*' -print -exec chown webmaster:webmaster {} \;
 
-sudo chown -R web-misc:web-misc /home/web-misc/
+sudo find /home/web-misc -not -user web-misc -not -group web-misc -print -exec chown web-misc:web-misc {} \;
 
-sudo chown -R steam:steam /home/steam/
+sudo find /home/steam -not -user steam -not -group steam -print -exec chown steam:steam {} \;
 
 # DISCORD
 
-sudo chown -R discord-fkz-1:discord-fkz-1 /home/discord-fkz-1/
+sudo find /home/discord-fkz-1 -not -user discord-fkz-1 -not -group discord-fkz-1 -print -exec chown discord-fkz-1:discord-fkz-1 {} \;
 
-sudo chown -R discord-fkz-2:discord-fkz-2 /home/discord-fkz-2/
+sudo find /home/discord-fkz-2 -not -user discord-fkz-2 -not -group discord-fkz-2 -print -exec chown discord-fkz-2:discord-fkz-2 {} \;
 
-sudo chown -R discord-fkz-3:discord-fkz-3 /home/discord-fkz-3/
+sudo find /home/discord-fkz-3 -not -user discord-fkz-3 -not -group discord-fkz-3 -print -exec chown discord-fkz-3:discord-fkz-3 {} \;
 
 # MC
 
-sudo chown -R mc-dot-1:mc-dot-1 /home/mc-dot-1/
+sudo find /home/mc-dot-1 -not -user mc-dot-1 -not -group mc-dot-1 -print -exec chown mc-dot-1:mc-dot-1 {} \;
 
-sudo chown -R mc-iwaki-1:mc-iwaki-1 /home/mc-iwaki-1/
+sudo find /home/mc-iwaki-1 -not -user mc-iwaki-1 -not -group mc-iwaki-1 -print -exec chown mc-iwaki-1:mc-iwaki-1 {} \;
 
 # CS2
 
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /home/cs2-fkz-1/
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files.femboy.kz/cs2/demos/fkz-1/
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files.femboy.kz/cs2/maps/
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files.femboy.kz/cs2/workshop/
+sudo find /home/cs2-fkz-1 -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
+sudo find /var/www/files.femboy.kz/cs2/demos/fkz-1 -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
+sudo find /var/www/files.femboy.kz/cs2/maps -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
+sudo find /var/www/files.femboy.kz/cs2/workshop -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
 
-sudo chown -R cs2-fkz-2:cs2-fkz-2 /home/cs2-fkz-2/
-sudo chown -R cs2-fkz-2:cs2-fkz-2 /var/www/files.femboy.kz/cs2/demos/fkz-2/
+sudo find /home/cs2-fkz-2 -not -user cs2-fkz-2 -not -group cs2-fkz-2 -print -exec chown cs2-fkz-2:cs2-fkz-2 {} \;
+sudo find /var/www/files.femboy.kz/cs2/demos/fkz-2 -not -user cs2-fkz-2 -not -group cs2-fkz-2 -print -exec chown cs2-fkz-2:cs2-fkz-2 {} \;
 
-sudo chown -R cs2-fkz-3:cs2-fkz-3 /home/cs2-fkz-3/
-sudo chown -R cs2-fkz-3:cs2-fkz-3 /var/www/files.femboy.kz/cs2/demos/fkz-3/
+sudo find /home/cs2-fkz-3 -not -user cs2-fkz-3 -not -group cs2-fkz-3 -print -exec chown cs2-fkz-3:cs2-fkz-3 {} \;
+sudo find /var/www/files.femboy.kz/cs2/demos/fkz-3 -not -user cs2-fkz-3 -not -group cs2-fkz-3 -print -exec chown cs2-fkz-3:cs2-fkz-3 {} \;
 
-sudo chown -R cs2-fkz-4:cs2-fkz-4 /home/cs2-fkz-4/
-sudo chown -R cs2-fkz-4:cs2-fkz-4 /var/www/files.femboy.kz/cs2/demos/fkz-4/
+sudo find /home/cs2-fkz-4 -not -user cs2-fkz-4 -not -group cs2-fkz-4 -print -exec chown cs2-fkz-4:cs2-fkz-4 {} \;
+sudo find /var/www/files.femboy.kz/cs2/demos/fkz-4 -not -user cs2-fkz-4 -not -group cs2-fkz-4 -print -exec chown cs2-fkz-4:cs2-fkz-4 {} \;
 
-sudo chown -R cs2-fkz-5:cs2-fkz-5 /home/cs2-fkz-5/
-sudo chown -R cs2-fkz-5:cs2-fkz-5 /var/www/files.femboy.kz/cs2/demos/fkz-5/
+sudo find /home/cs2-fkz-5 -not -user cs2-fkz-5 -not -group cs2-fkz-5 -print -exec chown cs2-fkz-5:cs2-fkz-5 {} \;
+sudo find /var/www/files.femboy.kz/cs2/demos/fkz-5 -not -user cs2-fkz-5 -not -group cs2-fkz-5 -print -exec chown cs2-fkz-5:cs2-fkz-5 {} \;
 
-sudo chown -R cs2-fkz-api:cs2-fkz-api /home/cs2-fkz-api/
+sudo find /home/cs2-fkz-api -not -user cs2-fkz-api -not -group cs2-fkz-api -print -exec chown cs2-fkz-api:cs2-fkz-api {} \;
 
 # CSGO
 
-sudo chown -R csgo-fkz-1:csgo-fkz-1 /home/csgo-fkz-1/
-sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-1/
-sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-1/
+sudo find /home/csgo-fkz-1 -not -user csgo-fkz-1 -not -group csgo-fkz-1 -print -exec chown csgo-fkz-1:csgo-fkz-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-1 -not -user csgo-fkz-1 -not -group csgo-fkz-1 -print -exec chown csgo-fkz-1:csgo-fkz-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-1 -not -user csgo-fkz-1 -not -group csgo-fkz-1 -print -exec chown csgo-fkz-1:csgo-fkz-1 {} \;
 
-sudo chown -R csgo-fkz-2:csgo-fkz-2 /home/csgo-fkz-2/
-sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-2/
-sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-2/
+sudo find /home/csgo-fkz-2 -not -user csgo-fkz-2 -not -group csgo-fkz-2 -print -exec chown csgo-fkz-2:csgo-fkz-2 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-2 -not -user csgo-fkz-2 -not -group csgo-fkz-2 -print -exec chown csgo-fkz-2:csgo-fkz-2 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-2 -not -user csgo-fkz-2 -not -group csgo-fkz-2 -print -exec chown csgo-fkz-2:csgo-fkz-2 {} \;
 
-sudo chown -R csgo-fkz-3:csgo-fkz-3 /home/csgo-fkz-3/
-sudo chown -R csgo-fkz-3:csgo-fkz-3 /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-3/
-sudo chown -R csgo-fkz-3:csgo-fkz-3 /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-3/
+sudo find /home/csgo-fkz-3 -not -user csgo-fkz-3 -not -group csgo-fkz-3 -print -exec chown csgo-fkz-3:csgo-fkz-3 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/fkz-3 -not -user csgo-fkz-3 -not -group csgo-fkz-3 -print -exec chown csgo-fkz-3:csgo-fkz-3 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/fkz-3 -not -user csgo-fkz-3 -not -group csgo-fkz-3 -print -exec chown csgo-fkz-3:csgo-fkz-3 {} \;
 
-sudo chown -R csgo-luf-1:csgo-luf-1 /home/csgo-luf-1/
-sudo chown -R csgo-luf-1:csgo-luf-1 /var/www/files.femboy.kz/fastdl/replays/gokz/luf-1/
-sudo chown -R csgo-luf-1:csgo-luf-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/luf-1/
+sudo find /home/csgo-luf-1 -not -user csgo-luf-1 -not -group csgo-luf-1 -print -exec chown csgo-luf-1:csgo-luf-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/luf-1 -not -user csgo-luf-1 -not -group csgo-luf-1 -print -exec chown csgo-luf-1:csgo-luf-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/luf-1 -not -user csgo-luf-1 -not -group csgo-luf-1 -print -exec chown csgo-luf-1:csgo-luf-1 {} \;
 
-sudo chown -R csgo-nikita-1:csgo-nikita-1 /home/csgo-nikita-1/
-sudo chown -R csgo-nikita-1:csgo-nikita-1 /var/www/files.femboy.kz/fastdl/replays/gokz/nikita-1/
-sudo chown -R csgo-nikita-1:csgo-nikita-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/nikita-1/
+sudo find /home/csgo-nikita-1 -not -user csgo-nikita-1 -not -group csgo-nikita-1 -print -exec chown csgo-nikita-1:csgo-nikita-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/nikita-1 -not -user csgo-nikita-1 -not -group csgo-nikita-1 -print -exec chown csgo-nikita-1:csgo-nikita-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/nikita-1 -not -user csgo-nikita-1 -not -group csgo-nikita-1 -print -exec chown csgo-nikita-1:csgo-nikita-1 {} \;
 
-sudo chown -R csgo-salad-1:csgo-salad-1 /home/csgo-salad-1/
-sudo chown -R csgo-salad-1:csgo-salad-1 /var/www/files.femboy.kz/fastdl/replays/gokz/fruity-1/
-sudo chown -R csgo-salad-1:csgo-salad-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/fruity-1/
+sudo find /home/csgo-salad-1 -not -user csgo-salad-1 -not -group csgo-salad-1 -print -exec chown csgo-salad-1:csgo-salad-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/fruity-1 -not -user csgo-salad-1 -not -group csgo-salad-1 -print -exec chown csgo-salad-1:csgo-salad-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/fruity-1 -not -user csgo-salad-1 -not -group csgo-salad-1 -print -exec chown csgo-salad-1:csgo-salad-1 {} \;
 
-sudo chown -R csgo-salad-2:csgo-salad-2 /home/csgo-salad-2/
-sudo chown -R csgo-salad-2:csgo-salad-2 /var/www/files.femboy.kz/fastdl/replays/gokz/fruity-2/
-sudo chown -R csgo-salad-2:csgo-salad-2 /var/www/files.femboy.kz/fastdl/replays/kztimer/fruity-2/
+sudo find /home/csgo-salad-2 -not -user csgo-salad-2 -not -group csgo-salad-2 -print -exec chown csgo-salad-2:csgo-salad-2 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/fruity-2 -not -user csgo-salad-2 -not -group csgo-salad-2 -print -exec chown csgo-salad-2:csgo-salad-2 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/fruity-2 -not -user csgo-salad-2 -not -group csgo-salad-2 -print -exec chown csgo-salad-2:csgo-salad-2 {} \;
 
-sudo chown -R csgo-somali-1:csgo-somali-1 /home/csgo-somali-1/
-sudo chown -R csgo-somali-1:csgo-somali-1 /var/www/files.femboy.kz/fastdl/replays/gokz/somali-1/
-sudo chown -R csgo-somali-1:csgo-somali-1 /var/www/files.femboy.kz/fastdl/replays/kztimer/somali-1/
+sudo find /home/csgo-somali-1 -not -user csgo-somali-1 -not -group csgo-somali-1 -print -exec chown csgo-somali-1:csgo-somali-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/somali-1 -not -user csgo-somali-1 -not -group csgo-somali-1 -print -exec chown csgo-somali-1:csgo-somali-1 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/somali-1 -not -user csgo-somali-1 -not -group csgo-somali-1 -print -exec chown csgo-somali-1:csgo-somali-1 {} \;
 
-sudo chown -R csgo-somali-2:csgo-somali-2 /home/csgo-somali-2/
-sudo chown -R csgo-somali-2:csgo-somali-2 /var/www/files.femboy.kz/fastdl/replays/gokz/somali-2/
-sudo chown -R csgo-somali-2:csgo-somali-2 /var/www/files.femboy.kz/fastdl/replays/kztimer/somali-2/
+sudo find /home/csgo-somali-2 -not -user csgo-somali-2 -not -group csgo-somali-2 -print -exec chown csgo-somali-2:csgo-somali-2 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/gokz/somali-2 -not -user csgo-somali-2 -not -group csgo-somali-2 -print -exec chown csgo-somali-2:csgo-somali-2 {} \;
+sudo find /var/www/files.femboy.kz/fastdl/replays/kztimer/somali-2 -not -user csgo-somali-2 -not -group csgo-somali-2 -print -exec chown csgo-somali-2:csgo-somali-2 {} \;
 
 # CSCL
 
-sudo chown -R cscl-fkz-1:cscl-fkz-1 /home/cscl-fkz-1/
-sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files.femboy.kz/cscl/replays/gokz/fkz-1/
-sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-1/
+sudo find /home/cscl-fkz-1 -not -user cscl-fkz-1 -not -group cscl-fkz-1 -print -exec chown cscl-fkz-1:cscl-fkz-1 {} \;
+sudo find /var/www/files.femboy.kz/cscl/replays/gokz/fkz-1 -not -user cscl-fkz-1 -not -group cscl-fkz-1 -print -exec chown cscl-fkz-1:cscl-fkz-1  {} \;
+sudo find /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-1 -not -user cscl-fkz-1 -not -group cscl-fkz-1 -print -exec chown cscl-fkz-1:cscl-fkz-1  {} \;
 
-sudo chown -R cscl-fkz-2:cscl-fkz-2 /home/cscl-fkz-2/
-sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files.femboy.kz/cscl/replays/gokz/fkz-2/
-sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-2/
+sudo find /home/cscl-fkz-2 -not -user cscl-fkz-2 -not -group cscl-fkz-2 -print -exec chown cscl-fkz-2:cscl-fkz-2 {} \;
+sudo find /var/www/files.femboy.kz/cscl/replays/gokz/fkz-2 -not -user cscl-fkz-2 -not -group cscl-fkz-2 -print -exec chown cscl-fkz-2:cscl-fkz-2  {} \;
+sudo find /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-2 -not -user cscl-fkz-2 -not -group cscl-fkz-2 -print -exec chown cscl-fkz-2:cscl-fkz-2  {} \;
 
-sudo chown -R cscl-fkz-3:cscl-fkz-3 /home/cscl-fkz-3/
-sudo chown -R cscl-fkz-3:cscl-fkz-3 /var/www/files.femboy.kz/cscl/replays/gokz/fkz-3/
-sudo chown -R cscl-fkz-3:cscl-fkz-3 /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-3/
+sudo find /home/cscl-fkz-3 -not -user cscl-fkz-3 -not -group cscl-fkz-3 -print -exec chown cscl-fkz-3:cscl-fkz-3 {} \;
+sudo find /var/www/files.femboy.kz/cscl/replays/gokz/fkz-3 -not -user cscl-fkz-3 -not -group cscl-fkz-3 -print -exec chown cscl-fkz-3:cscl-fkz-3  {} \;
+sudo find /var/www/files.femboy.kz/cscl/replays/kztimer/fkz-3 -not -user cscl-fkz-3 -not -group cscl-fkz-3 -print -exec chown cscl-fkz-3:cscl-fkz-3  {} \;
 
-echo "Ownership update complete."
+echo "Ownership check and update complete."
--- update_file_owners-na.sh
+++ update_file_owners-na.sh
@@ -2,48 +2,50 @@
 
 # MISC
 
-sudo chown -R webmaster:webmaster /home/webmaster/
-sudo chown -R webmaster:webmaster /var/www/
+sudo find /home/webmaster -not -user webmaster -not -group webmaster -print -exec chown webmaster:webmaster {} \;
+sudo find /var/www -not -user webmaster -not -group webmaster -not -path '/var/www/files-na.femboy.kz/cs2/demos*' -not -path '/var/www/files-na.femboy.kz/cs2/maps*' -not -path '/var/www/files-na.femboy.kz/cs2/workshop*' -not -path '/var/www/files-na.femboy.kz/cscl/replays*' -not -path '/var/www/files-na.femboy.kz/fastdl/replays*' -print -exec chown webmaster:webmaster {} \;
 
-sudo chown -R web-listen:web-listen /home/web-listen/
+sudo find /home/web-listen -not -user web-listen -not -group web-listen -print -exec chown web-listen:web-listen {} \;
 
-sudo chown -R debian:debian /home/debian/
+sudo find /home/debian -not -user debian -not -group debian -print -exec chown debian:debian {} \;
 
 # DISCORD
 
 # MC
 
+sudo find /home/mineraft -not -user mineraft -not -group mineraft -print -exec chown mineraft:mineraft {} \;
+
 # CS2
 
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /home/cs2-fkz-1/
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files-na.femboy.kz/cs2/demos/fkz-1/
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files-na.femboy.kz/cs2/maps/
-sudo chown -R cs2-fkz-1:cs2-fkz-1 /var/www/files-na.femboy.kz/cs2/workshop/
+sudo find /home/cs2-fkz-1 -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/cs2/demos/fkz-1 -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/cs2/maps -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/cs2/workshop -not -user cs2-fkz-1 -not -group cs2-fkz-1 -print -exec chown cs2-fkz-1:cs2-fkz-1 {} \;
 
-sudo chown -R cs2-fkz-2:cs2-fkz-2 /home/cs2-fkz-2/
-sudo chown -R cs2-fkz-2:cs2-fkz-2 /var/www/files-na.femboy.kz/cs2/demos/fkz-2/
+sudo find /home/cs2-fkz-2 -not -user cs2-fkz-2 -not -group cs2-fkz-2 -print -exec chown cs2-fkz-2:cs2-fkz-2 {} \;
+sudo find /var/www/files-na.femboy.kz/cs2/demos/fkz-2 -not -user cs2-fkz-2 -not -group cs2-fkz-2 -print -exec chown cs2-fkz-2:cs2-fkz-2 {} \;
 
-sudo chown -R cs2-fkz-3:cs2-fkz-3 /home/cs2-fkz-3/
-sudo chown -R cs2-fkz-3:cs2-fkz-3 /var/www/files-na.femboy.kz/cs2/demos/fkz-3/
+sudo find /home/cs2-fkz-3 -not -user cs2-fkz-3 -not -group cs2-fkz-3 -print -exec chown cs2-fkz-3:cs2-fkz-3 {} \;
+sudo find /var/www/files-na.femboy.kz/cs2/demos/fkz-3 -not -user cs2-fkz-3 -not -group cs2-fkz-3 -print -exec chown cs2-fkz-3:cs2-fkz-3 {} \;
 
 # CSGO
 
-sudo chown -R csgo-fkz-1:csgo-fkz-1 /home/csgo-fkz-1/
-sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files-na.femboy.kz/fastdl/replays/gokz/fkz-1/
-sudo chown -R csgo-fkz-1:csgo-fkz-1 /var/www/files-na.femboy.kz/fastdl/replays/kztimer/fkz-1/
+sudo find /home/csgo-fkz-1 -not -user csgo-fkz-1 -not -group csgo-fkz-1 -print -exec chown csgo-fkz-1:csgo-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/fastdl/replays/gokz/fkz-1 -not -user csgo-fkz-1 -not -group csgo-fkz-1 -print -exec chown csgo-fkz-1:csgo-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/fastdl/replays/kztimer/fkz-1 -not -user csgo-fkz-1 -not -group csgo-fkz-1 -print -exec chown csgo-fkz-1:csgo-fkz-1 {} \;
 
-sudo chown -R csgo-fkz-2:csgo-fkz-2 /home/csgo-fkz-2/
-sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files-na.femboy.kz/fastdl/replays/gokz/fkz-2/
-sudo chown -R csgo-fkz-2:csgo-fkz-2 /var/www/files-na.femboy.kz/fastdl/replays/kztimer/fkz-2/
+sudo find /home/csgo-fkz-2 -not -user csgo-fkz-2 -not -group csgo-fkz-2 -print -exec chown csgo-fkz-2:csgo-fkz-2 {} \;
+sudo find /var/www/files-na.femboy.kz/fastdl/replays/gokz/fkz-2 -not -user csgo-fkz-2 -not -group csgo-fkz-2 -print -exec chown csgo-fkz-2:csgo-fkz-2 {} \;
+sudo find /var/www/files-na.femboy.kz/fastdl/replays/kztimer/fkz-2 -not -user csgo-fkz-2 -not -group csgo-fkz-2 -print -exec chown csgo-fkz-2:csgo-fkz-2 {} \;
 
 # CSCL
 
-sudo chown -R cscl-fkz-1:cscl-fkz-1 /home/cscl-fkz-1/
-sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files-na.femboy.kz/cscl/replays/gokz/fkz-1/
-sudo chown -R cscl-fkz-1:cscl-fkz-1 /var/www/files-na.femboy.kz/cscl/replays/kztimer/fkz-1/
+sudo find /home/cscl-fkz-1 -not -user cscl-fkz-1 -not -group cscl-fkz-1 -print -exec chown cscl-fkz-1:cscl-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/cscl/replays/gokz/fkz-1 -not -user cscl-fkz-1 -not -group cscl-fkz-1 -print -exec chown cscl-fkz-1:cscl-fkz-1 {} \;
+sudo find /var/www/files-na.femboy.kz/cscl/replays/kztimer/fkz-1 -not -user cscl-fkz-1 -not -group cscl-fkz-1 -print -exec chown cscl-fkz-1:cscl-fkz-1 {} \;
 
-sudo chown -R cscl-fkz-2:cscl-fkz-2 /home/cscl-fkz-2/
-sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files-na.femboy.kz/cscl/replays/gokz/fkz-2/
-sudo chown -R cscl-fkz-2:cscl-fkz-2 /var/www/files-na.femboy.kz/cscl/replays/kztimer/fkz-2/
+sudo find /home/cscl-fkz-2 -not -user cscl-fkz-2 -not -group cscl-fkz-2 -print -exec chown cscl-fkz-2:cscl-fkz-2 {} \;
+sudo find /var/www/files-na.femboy.kz/cscl/replays/gokz/fkz-2 -not -user cscl-fkz-2 -not -group cscl-fkz-2 -print -exec chown cscl-fkz-2:cscl-fkz-2 {} \;
+sudo find /var/www/files-na.femboy.kz/cscl/replays/kztimer/fkz-2 -not -user cscl-fkz-2 -not -group cscl-fkz-2 -print -exec chown cscl-fkz-2:cscl-fkz-2 {} \;
 
-echo "Ownership update complete."
+echo "Ownership check and update complete."
