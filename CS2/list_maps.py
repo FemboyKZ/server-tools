0866aeeedfee9bc0b9cabb4b95871e2fe1f8c3fb
Dots <dotvnl@gmail.com>
2025-04-02 22:45:09 +0300

    script to list vpks into mapcycle file

---
+++ CS2/list_maps.py
@@ -0,0 +1,45 @@
+import os
+
+folder = "/var/www/files.femboy.kz/cs2/maps"
+output = "vpk_list.txt"
+skip_keywords = [
+    "vanity",
+    "preview",
+    "lobby",
+    "match",
+    "inspect",
+    "xpshop",
+    "icon",
+    "skybox",
+    "intro",
+    "template",
+    "backdrop",
+    "scene",
+    "smartprop",
+    "csgo_ui",
+    "settings",
+    "major",
+    "nametag",
+    "acknowledge_item",
+    "team_select",
+    "buy_menu",
+    "medal",
+]
+
+vpk_names = []
+
+for root, dirs, files in os.walk(folder):
+    for file in files:
+        if file.lower().endswith(".vpk"):
+            base_name = os.path.splitext(file)[0]
+            lower_name = base_name.lower()
+
+            if any(keyword in lower_name for keyword in skip_keywords):
+                continue
+
+            vpk_names.append(base_name)
+
+with open(output, "w") as f:
+    f.write("\n".join(vpk_names))
+
+print(f"Found {len(vpk_names)} VPK files, results saved to {output}")
