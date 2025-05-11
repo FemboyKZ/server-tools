d5506756a3515752c770763162ca852e65778d5c
Dots <dotvnl@gmail.com>
2025-05-11 20:10:54 +0300

    fix output

--- CS2/CS2RTVtoSA.py
+++ CS2/CS2RTVtoSA.py
@@ -18,10 +18,10 @@ def convert_maps(input, output):
             else:
                 default_maps.append(stripped_line)
 
-    output = {"DefaultMaps": default_maps, "WorkshopMaps": workshop_maps}
+    outputs = {"DefaultMaps": default_maps, "WorkshopMaps": workshop_maps}
 
-    with open("maplist.json", "w") as json_file:
-        json.dump(output, json_file, indent=2)
+    with open(output, "w") as json_file:
+        json.dump(outputs, json_file, indent=2)
 
 
 if __name__ == "__main__":
